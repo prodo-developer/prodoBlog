c5cfcb39160e6642d11cb999a96b07e72d6706a6
prodo-developer <wjdals300@gmail.com>
2022-06-28 23:42:17 +0900

    refactor : @NotBlank 추가

--- src/main/java/com/prodoblog/request/PostCreate.java
+++ src/main/java/com/prodoblog/request/PostCreate.java
@@ -4,12 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @ToString
 public class PostCreate {
 
+    //@Valid에서 체크한 기준으로 빈값을 체크한다. null도 체크해줌.
+    @NotBlank(message = "타이틀을 입력해주세요")
     public String title;
+
+    @NotBlank(message = "내용을 입력해주세요")
     public String content;
 
 }
