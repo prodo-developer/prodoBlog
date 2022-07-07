59f8cfae667d2dee1fb48772c2c37a4d5ac9e65d
prodo-developer <wjdals300@gmail.com>
2022-07-07 23:26:32 +0900

    refactor: 데이터 검증 수정

--- src/main/java/com/prodoblog/request/PostCreate.java
+++ src/main/java/com/prodoblog/request/PostCreate.java
@@ -12,10 +12,10 @@ import javax.validation.constraints.NotBlank;
 public class PostCreate {
 
     //@Valid에서 체크한 기준으로 빈값을 체크한다. null도 체크해줌.
-    @NotBlank(message = "타이틀을 입력해주세요")
+    @NotBlank(message = "타이틀을 입력해주세요.")
     public String title;
 
-    @NotBlank(message = "내용을 입력해주세요")
+    @NotBlank(message = "내용을 입력해주세요.")
     public String content;
 
 }
