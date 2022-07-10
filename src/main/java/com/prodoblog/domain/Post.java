27b1c56585020c839a9fb149bb041bde07ebb706
prodo-developer <wjdals300@gmail.com>
2022-07-10 15:40:58 +0900

    feat: Post Entity ADD

---
+++ src/main/java/com/prodoblog/domain/Post.java
@@ -0,0 +1,25 @@
+package com.prodoblog.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
