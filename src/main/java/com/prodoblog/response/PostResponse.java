a5c9d4d853dccfe8185589f35a7553ae53f194dd
prodo-developer <wjdals300@gmail.com>
2022-07-23 11:41:26 +0900

    feat: PostResponse 추가

---
+++ src/main/java/com/prodoblog/response/PostResponse.java
@@ -0,0 +1,28 @@
+package com.prodoblog.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+//@Builder
+//@RequiredArgsConstructor
+public class PostResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    // 방법1 빌더를 직접 메서드에 달아주고 필터링하기
+    @Builder
+    public PostResponse(Long id, String title, String content) {
+        this.id = id;
+//        this.title = title.substring(0,10);
+        this.title = title.substring(0,Math.min(title.length(), 10));
+        this.content = content;
+    }
+
+    // 방법2 전역변수에 @Builder달아주고, 직접필터링하기
+//    public String getTitle() {
+//        return this.title.substring(0,10);
+//    }
+}
